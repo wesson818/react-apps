dec223c10b26adf8a60abfc013bcfd848a9bbbd5
WenJing Zhang <wesson818@gmail.com>
2020-07-23 09:45:28 +1000

    finish scrimba react

--- scrimba/my-app/src/App.js
+++ scrimba/my-app/src/App.js
@@ -3,14 +3,45 @@ import './App.css';
 import Header from './components/Header';
 import MyInfo from './components/MyInfo';
 import Footer from './components/Footer';
+import ContactCard from './components/ContactCard';
+import Counter from './components/Counter';
+import Joke from './components/Joke';
+import Product from './components/Product';
+import jokesData from './data/jokesData';
+import productsData from './data/vschoolProducts';
 import Form from './Form';
+import randomColor from 'randomcolor'
 
 class App extends Component {
   constructor(){
     super()
     this.state={
       loading: false,
-      character: {}
+      character: {},
+      count:0,
+      color:""
+    }
+    this.countAdd = this.countAdd.bind(this)
+    this.countMin = this.countMin.bind(this)
+  }
+  countAdd(){
+    this.setState(prevState => {
+        return {
+            count: prevState.count + 1
+        }
+    })
+  }
+  countMin(){
+    this.setState(prevState => {
+        return {
+            count: prevState.count - 1
+        }
+    })
+  }
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.count!=this.state.count){
+      const newColor = randomColor()
+      this.setState({color: newColor})
     }
   }
 
@@ -28,12 +59,50 @@ class App extends Component {
 
   
   render(){
+    
+    //Joke
+    const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+
+    //Product map
+    // const productComponents = productsData.map(product => <Product key={product.id} name={product.name} price={product.price} description={product.description} />)
+
+    //Product filter
+    const freeProductComponents = productsData.filter(product => 
+      product.price === 0
+    ).map(product => 
+      <Product key={product.id} name={product.name} price={product.price} description={product.description} />
+    )
+    
     return (
       <div className="App App-header">
         <Header />
         {this.state.loading?"Loading...":this.state.character.name}
+        <Counter color={this.state.color} count={this.state.count} countAdd={this.countAdd} countMin={this.countMin} />
         <Form />
         <MyInfo />
+        <div className="contacts">
+              <ContactCard 
+                  contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
+              />
+              
+              <ContactCard 
+                  contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
+              />
+              
+              <ContactCard
+                  contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
+              />
+              
+              <ContactCard 
+                  contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
+              />
+          </div>
+          <div>
+            {jokeComponents}
+          </div>
+          <div>
+            {freeProductComponents}
+          </div>
         <Footer />
       </div>
     );
--- scrimba/todo-list/src/App.js
+++ scrimba/todo-list/src/App.js
@@ -1,54 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem'
-import ContactCard from './components/ContactCard'
-import Joke from "./components/Joke"
-import jokesData from './data/jokesData';
-import Product from './components/Product';
-import productsData from './data/vschoolProducts';
 import todoData from './data/todoData';
-import Counter from './components/Counter';
-import randomColor from 'randomcolor'
  
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      todos:todoData,
-      count:0,
-      color:"",
-      isLoading: true
+      todos:todoData
     }
     this.handleChange = this.handleChange.bind(this)
-    this.countAdd = this.countAdd.bind(this)
-    this.countMin = this.countMin.bind(this)
-  }
-  componentDidMount() {
-    setTimeout(() => {
-        this.setState({
-            isLoading: false
-          })
-      }, 1500)
-  }
-  countAdd(){
-    this.setState(prevState => {
-        return {
-            count: prevState.count + 1
-        }
-    })
-  }
-  countMin(){
-    this.setState(prevState => {
-        return {
-            count: prevState.count - 1
-        }
-    })
-  }
-  componentDidUpdate(prevProps, prevState){
-    if(prevState.count!=this.state.count){
-      const newColor = randomColor()
-      this.setState({color: newColor})
-    }
   }
   handleChange(id){
     console.log("id",id)
@@ -99,57 +60,15 @@ class App extends Component {
   //List todo
   const todoComponent = this.state.todos.map(todo => <TodoItem key={todo.id} id={todo.id} isChecked={todo.completed} todoText={todo.text} handleChange={this.handleChange} />)
 
-  //Joke
-  const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
-
-  //Product map
-  // const productComponents = productsData.map(product => <Product key={product.id} name={product.name} price={product.price} description={product.description} />)
-
-  //Product filter
-  const freeProductComponents = productsData.filter(product => 
-    product.price === 0
-  ).map(product => 
-    <Product key={product.id} name={product.name} price={product.price} description={product.description} />
-  )
   return (
     <div className="App App-header">
-      {this.state.isLoading? 
-      <h2>Loading...</h2>:<h2 style={h2Style}>Good {timeOfDay}  {firstname} {lastname}</h2>}
-      <Counter color={this.state.color} count={this.state.count} countAdd={this.countAdd} countMin={this.countMin} />
-
-      {/* <h2 style={h2Style}>Good {hours < 12?"morning":"afternoon"}  {firstname} {lastname}</h2> */}
-      {/* <h2>Hi {`${firstname} ${lastname}`}</h2> */}
+      <h2 style={h2Style}>Good {timeOfDay}  {firstname} {lastname}</h2>
       <h3>Todo List</h3>
       <div className="todo-list">
         {todoComponent}
       </div>
-      <div className="contacts">
-            <ContactCard 
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-            />
-            
-            <ContactCard 
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-            />
-            
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-            />
-            
-            <ContactCard 
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            />
-        </div>
-        <div>
-           {jokeComponents}
-        </div>
-        <div>
-          {freeProductComponents}
-        </div>
     </div>
   );
 }}
-
-
 
 export default App;
